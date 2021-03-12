5b9f4074f0cca494848eb49d651a42224b710055
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-12 20:07:23 +0000

    Move ExtractSuperclassBefore out to its own file

--- RefactoringSamples/Extract/ExtractSuperclass.cs
+++ RefactoringSamples/Extract/ExtractSuperclass.cs
@@ -1,18 +1,5 @@
 ﻿namespace RefactoringSamples.Extract
 {
-    public class ExtractSuperclassBefore
-    {
-        int method1()
-        {
-            return 0;
-        }
-        
-        int method2()
-        {
-            return 0;
-        }
-    }
-
     public class ExtractSuperclassAfterBase
     {
         private int method1()
---
+++ RefactoringSamples/Extract/ExtractSuperclassBefore.cs
@@ -0,0 +1,15 @@
+﻿namespace RefactoringSamples.Extract
+{
+    public class ExtractSuperclassBefore
+    {
+        int method1()
+        {
+            return 0;
+        }
+        
+        int method2()
+        {
+            return 0;
+        }
+    }
+}
