44b14e038afc9554e47c6f0f69b76f91f7523a01
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-12 18:23:20 +0000

    Add ExtractSuperclass example

---
+++ RefactoringSamples/Extract/ExtractSuperclass.cs
@@ -0,0 +1,32 @@
+﻿namespace RefactoringSamples
+{
+    public class ExtractSuperclassBefore
+    {
+        int method1()
+        {
+            return 0;
+        }
+        
+        int method2()
+        {
+            return 0;
+        }
+    }
+
+    public class ExtractSuperclassAfterBase
+    {
+        private int method1()
+        {
+            return 0;
+        }
+
+        private int method2()
+        {
+            return 0;
+        }
+    }
+
+    public class ExtractSuperclassAfter : ExtractSuperclassAfterBase
+    {
+    }
+}
