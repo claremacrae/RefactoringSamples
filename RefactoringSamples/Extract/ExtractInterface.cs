30570d3cdfe0e305de8d9842318bd91297461d58
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-12 18:30:51 +0000

    Add ExtractInterface example

---
+++ RefactoringSamples/Extract/ExtractInterface.cs
@@ -0,0 +1,34 @@
+﻿namespace RefactoringSamples
+{
+    public class ExtractInterfaceBefore
+    {
+        int method1()
+        {
+            return 0;
+        }
+        
+        int method2()
+        {
+            return 0;
+        }
+    }
+
+    public interface IExtractInterfaceAfter
+    {
+        int method1();
+        int method2();
+    }
+
+    public class ExtractInterfaceAfter : IExtractInterfaceAfter
+    {
+        public int method1()
+        {
+            return 0;
+        }
+
+        public int method2()
+        {
+            return 0;
+        }
+    }
+}
