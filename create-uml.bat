a678ef7c89ba618f11772a2939301d4beb8294b6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-12 19:13:10 +0000

    Move ExtractFieldBefore.cs to subfolder called Before

--- create-uml.bat
+++ create-uml.bat
@@ -3,4 +3,5 @@ del /s /q *.svg
 puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out
 plantuml -tsvg uml\*.puml
 plantuml -tsvg uml\Extract\*.puml
+plantuml -tsvg uml\Extract\Before\*.puml
 plantuml -tsvg uml\Keys\*.puml
