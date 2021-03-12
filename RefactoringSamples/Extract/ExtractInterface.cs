461dd7b776b75a404f0574cb21fc166ac542bd7a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-12 20:05:12 +0000

    Move class to separate file ExtractInterfaceBefore.cs

--- RefactoringSamples/Extract/ExtractInterface.cs
+++ RefactoringSamples/Extract/ExtractInterface.cs
@@ -1,18 +1,5 @@
 ﻿namespace RefactoringSamples.Extract
 {
-    public class ExtractInterfaceBefore
-    {
-        int method1()
-        {
-            return 0;
-        }
-        
-        int method2()
-        {
-            return 0;
-        }
-    }
-
     public interface IExtractInterfaceAfter
     {
         int method1();
---
+++ RefactoringSamples/Extract/ExtractInterfaceBefore.cs
@@ -0,0 +1,15 @@
+﻿namespace RefactoringSamples.Extract
+{
+    public class ExtractInterfaceBefore
+    {
+        int method1()
+        {
+            return 0;
+        }
+        
+        int method2()
+        {
+            return 0;
+        }
+    }
+}
