afe51e39d6c09d303839afbf143c90867971fe61
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-12 21:31:48 +0000

    Removed need to manually list subdirectories in create-uml.bat

--- create-uml.bat
+++ create-uml.bat
@@ -1,8 +1,5 @@
 del /s /q *.puml
 del /s /q *.svg
 puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out
-plantuml -tsvg uml\*.puml
-plantuml -tsvg uml\Extract\*.puml
-plantuml -tsvg uml\Extract\Before\*.puml
-plantuml -tsvg uml\Extract\After\*.puml
-plantuml -tsvg uml\Keys\*.puml
+
+plantuml -tsvg "uml\**.puml"
