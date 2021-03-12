a00ac99c17ac0bd72a76410155738bc0cc4cbc91
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-12 19:15:04 +0000

    Move ExtractFieldAfter.cs to subfolder called After

--- create-uml.bat
+++ create-uml.bat
@@ -4,4 +4,5 @@ puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out
 plantuml -tsvg uml\*.puml
 plantuml -tsvg uml\Extract\*.puml
 plantuml -tsvg uml\Extract\Before\*.puml
+plantuml -tsvg uml\Extract\After\*.puml
 plantuml -tsvg uml\Keys\*.puml
