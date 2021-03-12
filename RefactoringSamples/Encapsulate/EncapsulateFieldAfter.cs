a8c4d32a063fa652fd18c967515fa78f3b606c57
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-12 20:27:23 +0000

    Move types in to matching files

--- RefactoringSamples/Encapsulate/EncapsulateFieldAfter.cs
+++ RefactoringSamples/Encapsulate/EncapsulateFieldAfter.cs
@@ -1,13 +1,4 @@
-﻿namespace RefactoringSamples.Encapsulate.Before
-{
-    public class EncapsulateFieldBefore
-    {
-        private int _thing1;
-        private int _thing2;
-    }
-}
-
-namespace RefactoringSamples.Encapsulate.After
+﻿namespace RefactoringSamples.Encapsulate.After
 {
     public class EncapsulateFieldAfter
     {
---
+++ RefactoringSamples/Encapsulate/EncapsulateFieldBefore.cs
@@ -0,0 +1,8 @@
+﻿namespace RefactoringSamples.Encapsulate.Before
+{
+    public class EncapsulateFieldBefore
+    {
+        private int _thing1;
+        private int _thing2;
+    }
+}
