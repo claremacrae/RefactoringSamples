14152ddf0b7e961953a3e36086c1e666617d1d77
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-13 16:42:41 +0000

    Delete include.puml, as it gives an error when processed on Mac

--- create-uml.sh
+++ create-uml.sh
@@ -3,4 +3,9 @@
 find . -name \*.puml -o -name \*.svg -exec rm {} \;
 puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out | grep -v "^Processing "
 
+# Delete a file which gives an error on Mac:
+#   Error line 2 in file: uml/include.puml
+#   Some diagram description contains errors
+rm uml/include.puml
+
 plantuml -tsvg 'uml/**.puml'
