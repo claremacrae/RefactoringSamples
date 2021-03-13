b474377dcf6fca139d141de6b7358d03ec44b84b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-13 12:13:08 +0000

    Switch to mdsnippets convention "SourceTransform"
    So I'll now edit the mdsource/*.source.md files.
    
    This will make them easier to edit.

---
+++ run_markdown_templates.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# ---------------------------------------------------
+# Update code examples
+# ---------------------------------------------------
+# For info on mdsnippets, see https://github.com/SimonCropp/MarkdownSnippets
+
+# install dotnet SDK from http://go.microsoft.com/fwlink/?LinkID=798306&clcid=0x409
+# Then install MarkdownSnippets.Tool with
+#   dotnet tool install -g MarkdownSnippets.Tool
+# To update:
+#   dotnet tool update  -g MarkdownSnippets.Tool
+# To uninstall (e.g. to downgrade to a lower version)
+# dotnet tool uninstall -g MarkdownSnippets.Tool
+
+#
+#   See https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/BuildingDocumentation.md#top
+dotnet tool update  -g MarkdownSnippets.Tool --version 23.0.0 || exit
+mdsnippets || exit 1
