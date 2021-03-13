9159246e53586b8a2f4c9bc4797bb0c6ff2d8a51
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-13 16:35:41 +0000

    Strip out the "Processing " output from puml-gen output

--- create-uml.sh
+++ create-uml.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 find . -name \*.puml -o -name \*.svg -exec rm {} \;
-puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out
+puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out | grep -v "^Processing "
 
 plantuml -tsvg 'uml/**.puml'
