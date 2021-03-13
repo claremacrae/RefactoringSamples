1298b6f3a58c5b7c08428b6bde977134d001e6cc
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-13 20:25:06 +0000

    Don't filter out "Processing" lines, as this triggers halt-on-error

--- create-uml.sh
+++ create-uml.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Force execution to halt if there are any errors in this script:
-#set -e
-#set -o pipefail
+set -e
+set -o pipefail
 
 find . \( -name \*.puml -o -name \*.svg \) -exec rm {} \;
-puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out | grep -v "^Processing "
+puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out
 
 # Delete a file which gives an error on Mac:
 #   Error line 2 in file: uml/include.puml
