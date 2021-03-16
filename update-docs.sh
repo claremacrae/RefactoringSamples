0bd7ea156e2aad4b8d30fbbff10f8ac9fca9411a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-16 12:03:04 +0000

    Turn off auto-creating of files for now, due to Python module error

--- update-docs.sh
+++ update-docs.sh
@@ -4,9 +4,14 @@
 set -e
 set -o pipefail
 
-cd  scripts
-./create-pattern.py
-cd ..
+# TODO Fix failure to find scripts module
+# Traceback (most recent call last):
+#  File "./create-pattern.py", line 5, in <module>
+#    from scripts.constants import do_not_overwrite, overwrite_if_exists
+#ModuleNotFoundError: No module named 'scripts'
+#cd  scripts
+#./create-pattern.py
+#cd ..
 
 ./create-uml.sh
 ./run_markdown_templates.sh
