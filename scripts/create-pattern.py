28267b3a49ce490b781e4ed3f2d0296e7333abc3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-16 11:33:31 +0000

    Move FileFromTemplate out to separate file

--- scripts/create-pattern.py
+++ scripts/create-pattern.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-import os
-
 from jinja2 import Environment, FileSystemLoader
 
 from scripts.constants import do_not_overwrite, overwrite_if_exists
+from scripts.file_from_template import FileFromTemplate
 from scripts.inputs import Inputs
 from scripts.outputs import Outputs
 
@@ -15,35 +14,6 @@ def create_files(category, source_files):
     for source_file in source_files:
         refactoring['source_file'] = source_file
         create_files_impl(refactoring)
-
-
-class FileFromTemplate(object):
-    def __init__(self, inputs, outputs):
-        self.inputs = inputs
-        self.outputs = outputs
-
-    def create_file(self, refactoring, env):
-        output_directory = self.render_template(env, refactoring, self.outputs.directory_template)
-        os.makedirs(output_directory, exist_ok=True)
-        output_file = self.render_template(env, refactoring, self.outputs.file_template)
-        output_file_name = os.path.join(output_directory, output_file)
-
-        self.expand_template(env, output_file_name, refactoring)
-
-    def expand_template(self, env, output_file_name, refactoring):
-        output = self.render_template(env, refactoring, self.inputs.template_name)
-        self.write_file(output, output_file_name)
-
-    def write_file(self, output, output_file_name):
-        if os.path.exists(output_file_name) and self.inputs.overwrite_if_existing == do_not_overwrite:
-            print(f'File already exists: not overwriting: {output_file_name}')
-            return
-        with open(output_file_name, 'w') as f:
-            f.write(output)
-
-    def render_template(self, env, refactoring, template_name):
-        template = env.get_template(template_name)
-        return template.render(data=refactoring, extension = self.inputs.extension, stage=self.inputs.stage)
 
 
 class OutputFiles:
