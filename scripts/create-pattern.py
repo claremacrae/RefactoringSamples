571d38def3069ef2cf1b4cb9b15033885a7b08a4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-17 11:56:18 +0000

    Move AllRefactorings to all_refactorings.py

--- scripts/create-pattern.py
+++ scripts/create-pattern.py
@@ -1,45 +1,9 @@
 #!/usr/bin/env python3
-from typing import List
-
-from scripts.constants import overwrite_if_exists
-from scripts.file_from_template import FileFromTemplate
-from scripts.inputs import Inputs
-from scripts.outputs import Outputs
+from scripts.all_refactorings import AllRefactorings
 from scripts.refactoring import Refactoring
 from scripts.refactoring_category import RefactoringCategory
 
 Refactoring.templates = Refactoring.create_templates()
-
-
-class AllRefactorings:
-    def __init__(self):
-        self.categories: List[RefactoringCategory] = []
-
-    def add_category(self, category: RefactoringCategory):
-        self.categories.append(category)
-
-    def create_files(self):
-        self.create_doc_readme()
-        self.create_per_category_files()
-
-    def create_per_category_files(self):
-        for category in self.categories:
-            category.create_files()
-
-    def create_doc_readme(self):
-        inputs = Inputs('doc-readme.md', 'source.md', '', overwrite_if_exists)
-        outputs = Outputs('../docs/mdsource')
-        template = FileFromTemplate(inputs, outputs)
-
-        template.create_file(self.as_dictionary())
-
-    def as_dictionary(self):
-        refactoring = dict()
-        refactoring['source_file'] = 'README'
-        refactoring['categories_and_titles'] = zip([x.category for x in self.categories],
-                                                   [x.title for x in self.categories])
-        return refactoring
-
 
 if __name__ == '__main__':
     all = AllRefactorings()
