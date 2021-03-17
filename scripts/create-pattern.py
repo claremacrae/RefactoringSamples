1e842b4cd992d80cd8793b67e0de4ed8fc6be5ca
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-17 13:33:17 +0000

    Reformat code for readability

--- scripts/create-pattern.py
+++ scripts/create-pattern.py
@@ -4,15 +4,26 @@ from scripts.refactoring_category import RefactoringCategory
 
 if __name__ == '__main__':
     all = AllRefactorings()
-    all.add_category(RefactoringCategory('Encapsulate', [
-        'Encapsulate Field', ]))
-    all.add_category(RefactoringCategory('Extract', [
-        'Extract Class',
-        'Extract Interface',
-        'Extract Superclass', ]))
-    all.add_category(RefactoringCategory('If Statements', [
-        'Remove Redundant Else', ]))
-    all.add_category(RefactoringCategory('Introduce', [
-        'Introduce Field', ]))
+    all.add_category(
+        RefactoringCategory('Encapsulate', [
+            'Encapsulate Field',
+        ]))
+
+    all.add_category(
+        RefactoringCategory('Extract', [
+            'Extract Class',
+            'Extract Interface',
+            'Extract Superclass',
+        ]))
+
+    all.add_category(
+        RefactoringCategory('If Statements', [
+            'Remove Redundant Else',
+        ]))
+
+    all.add_category(
+        RefactoringCategory('Introduce', [
+            'Introduce Field',
+        ]))
 
     all.create_files()
