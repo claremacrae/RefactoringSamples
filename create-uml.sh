3f162b00b145080b217ac1b537d66f60095bb42f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-18 08:50:39 +0000

    Add comment about platuml's -createAssociation option

--- create-uml.sh
+++ create-uml.sh
@@ -5,6 +5,9 @@ set -e
 set -o pipefail
 
 # https://github.com/pierre3/PlantUmlClassDiagramGenerator
+# Option -createAssociation looked like it would be useful, but for
+# ExtractClass.svg, the layout was terrible, with _concept2 overlapping
+# the Concept2 class
 puml-gen RefactoringSamples uml -dir -excludePaths RefactoringSamples\out
 
 # Delete a file which gives an error on Mac:
