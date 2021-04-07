f044010f5d2aa81a91b2fbb717b948ae6340f79e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-07 12:55:04 +0100

    Add some missing type hints

--- scripts/file_from_template.py
+++ scripts/file_from_template.py
@@ -11,12 +11,12 @@ class FileFromTemplate(object):
     file_loader = FileSystemLoader('../templates')
     env = Environment(loader=file_loader)
 
-    def __init__(self, inputs: Inputs, outputs: Outputs):
+    def __init__(self, inputs: Inputs, outputs: Outputs) -> None:
 
         self.inputs = inputs
         self.outputs = outputs
 
-    def create_file(self, refactoring):
+    def create_file(self, refactoring) -> None:
         output_directory = self.render_string_template(refactoring, self.outputs.directory_template)
         os.makedirs(output_directory, exist_ok=True)
         output_file = self.render_string_template(refactoring, self.outputs.file_name_template)
@@ -24,24 +24,24 @@ class FileFromTemplate(object):
 
         self.expand_template(output_file_name, refactoring)
 
-    def expand_template(self, output_file_name, refactoring):
+    def expand_template(self, output_file_name, refactoring) -> None:
         output = self.render_file_template(refactoring, self.inputs.template_name)
         self.write_file(output, output_file_name)
 
-    def write_file(self, output, output_file_name):
+    def write_file(self, output, output_file_name) -> None:
         if os.path.exists(output_file_name) and self.inputs.overwrite_if_existing == do_not_overwrite:
             print(f'File already exists: not overwriting: {output_file_name}')
             return
         with open(output_file_name, 'w') as f:
             f.write(output)
 
-    def render_file_template(self, refactoring, template_name):
+    def render_file_template(self, refactoring, template_name) -> str:
         template = FileFromTemplate.env.get_template(template_name)
         return self.render_template(refactoring, template)
 
-    def render_string_template(self, refactoring, template_text):
+    def render_string_template(self, refactoring, template_text) -> str:
         template = Template(template_text)
         return self.render_template(refactoring, template)
 
-    def render_template(self, refactoring, template):
+    def render_template(self, refactoring, template) -> str:
         return template.render(data=refactoring, extension=self.inputs.extension, stage=self.inputs.stage)
