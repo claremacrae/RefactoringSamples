96d8c316b9ed63c75e030ff93f5aa3b749e5a1bd
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-07 12:50:43 +0100

    Add some missing type hints

--- scripts/all_refactorings.py
+++ scripts/all_refactorings.py
@@ -8,21 +8,21 @@ from scripts.refactoring_category import RefactoringCategory
 
 
 class AllRefactorings:
-    def __init__(self):
+    def __init__(self) -> None:
         self.categories: List[RefactoringCategory] = []
 
-    def add_category(self, category: RefactoringCategory):
+    def add_category(self, category: RefactoringCategory) -> None:
         self.categories.append(category)
 
-    def create_files(self):
+    def create_files(self) -> None:
         self.create_doc_readme()
         self.create_per_category_files()
 
-    def create_per_category_files(self):
+    def create_per_category_files(self) -> None:
         for category in self.categories:
             category.create_files()
 
-    def create_doc_readme(self):
+    def create_doc_readme(self) -> None:
         inputs = Inputs('doc-readme.md', 'source.md', '', overwrite_if_exists)
         outputs = Outputs('../docs/mdsource')
         template = FileFromTemplate(inputs, outputs)
